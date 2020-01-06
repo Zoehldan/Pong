13ae156632033ed726b8420c776bcbdc8f935930
zoehldan <zoehldan000@warren.k12.in.us>
2020-01-06 13:04:19 -0500

    added comment

--- src/Board.java
+++ src/Board.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
+//This class paints and moves objects for the game of Pong
 public class Board extends JPanel implements ActionListener {
     final int WIDTH=800;
     final int HEIGHT=600;
